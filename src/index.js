2c6e7db912728d633e48880b8835c0a6e3dcd381
ArthurDjikpo <arthur.djikpo@gmail.com>
2020-02-16 19:41:11 +0100

    add a new file structure, delete the old directory src

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from "./components/app";
-import reducers from "./reducers";
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider
-    store={createStoreWithMiddleware(
-      reducers,
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )}
-  >
-    <App />
-  </Provider>,
-  document.querySelector(".container")
-);
+import App from './containers/app'
+ReactDOM.render(<App/>,document.querySelector('.container'));
