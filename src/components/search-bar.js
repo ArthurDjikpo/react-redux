65c956684ce3e90e84ba5a2e05b421f45d079b73
ArthurDjikpo <arthur.djikpo@gmail.com>
2020-02-16 20:35:18 +0100

    update the searchBar.js

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const SearchBar = ()=>{
+    const [SearchText, SetSearchText] = useState()
+    const placeHolder = 'Tapez votre film ....'
+
+    const handleChange = (e) =>{
+        SetSearchText(e.target.value)
+        console.log(SearchText)
+    }
     
-    return <input/>
+    return (
+        <div>
+            <input
+                onChange={handleChange}
+                placeholder={placeHolder}
+            />
+            <p>{SearchText}</p>
+        </div>
+    )
 }
 
 export default SearchBar;
